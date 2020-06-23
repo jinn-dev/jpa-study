198e19e656441e54f7dc90617af9789f69b2b8a1
jinn-dev <gsbyyy@naver.com>
2020-06-23 21:38:49 +0900

    상품 엔티티에 비즈니스 로직 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -13,6 +13,7 @@ import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -35,5 +36,19 @@ public abstract class Item {
 	
 	@ManyToMany(mappedBy = "items")
 	private List<Category> categories = new ArrayList<Category>();
+
+	//비즈니스 로직
+	//Setter를 안쓰고 내부처리를 이렇게 해줄수도 있다는걸 보여줌
+	public void addStock(int quantity) {
+		this.stockQuantity += quantity;
+	}
 	
+	public void removeStock(int quantity) {
+		int restStock = this.stockQuantity - quantity;
+		if(restStock < 0) {
+			throw new NotEnoughStockException("need more stock!!");
+		}
+		this.stockQuantity = restStock; 
+		
+	}
 }
